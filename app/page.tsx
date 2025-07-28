29e14b220078a3e194cca10684c6ebbbe5a25a6e
Arun <arunkumarrvenkatesh@gmail.com>
2025-07-28 18:30:56 +0530

    Describe your changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@
 import Header from '../components/Header';
 import HeroSection from '../components/HeroSection';
 import AboutSection from '../components/AboutSection';
-import TestimonialsSection from '../components/TestimonialsSection';
+
 import ProjectsSection from '../components/ProjectsSection';
 import MiniGallery from '../components/MiniGallery';
 import ContactSection from '../components/ContactSection';
@@ -17,7 +17,7 @@ export default function Home() {
       <main>
         <HeroSection />
         <AboutSection />
-        <TestimonialsSection />
+    
         <ProjectsSection />
         <MiniGallery />
         <ContactSection />
